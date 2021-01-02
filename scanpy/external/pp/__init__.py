a55bea36cab5d3ff0b4111992ee22f91314b6a18
afvallejo <36366130+afvallejo@users.noreply.github.com>
2021-01-02 20:43:32 +0000

    Update __init__.py

--- scanpy/external/pp/__init__.py
+++ scanpy/external/pp/__init__.py
@@ -6,3 +6,4 @@ from ._magic import magic
 from ._scvi import scvi
 from ._hashsolo import hashsolo
 from ._scrublet import scrublet, scrublet_simulate_doublets
+from ._sctransform import sctransform
